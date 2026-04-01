5068c40ce8bb397d75fa022700fee1769fc8514c
Tanyathorn <tanyathorn.k@ku.th>
2026-04-02 03:57:23 +0700

    Fixed Aspect Ration in config

--- rt_camera.py
+++ rt_camera.py
@@ -1,6 +1,6 @@
 import taichi as ti
 import numpy as np
-from rt_config import width, height, vec3
+from rt_config import width, height, vec3, aspect_ratio
 
 look_from = vec3(0.0, 15.0, 35.0)
 look_at = vec3(0.0, 0.0, 0.0)
@@ -10,7 +10,7 @@ fov = 60.0
 theta = fov * np.pi / 180.0
 h = np.tan(theta / 2.0)
 viewport_height = 2.0 * h
-viewport_width = viewport_height * (width / height)
+viewport_width = viewport_height * aspect_ratio
 w = (look_from - look_at).normalized()
 u = vup.cross(w).normalized()
 v = w.cross(u)
